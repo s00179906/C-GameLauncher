925a639838252e6a8c032ddbe8475f1565664959
hignz <higz-06@hotmail.com>
2019-06-11 17:30:19 +0100

    game folder selection support

--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -1,27 +1,30 @@
-﻿using System;
+﻿using GameLauncher.Models;
+using GameLauncher.Utils;
+using Microsoft.WindowsAPICodePack.Dialogs;
+using System;
+using System.Collections.ObjectModel;
 using System.Linq;
 
 namespace GameLauncher.ViewModels
 {
     public class MainViewModel
     {
+        public ObservableCollection<Game> Games { get; set; }
+        public ObservableCollection<string> FolderPaths { get; set; }
+        public CommandRunner AddFolderPathCommand { get; set; }
+
 
         public MainViewModel()
         {
             Console.WriteLine("View Model Initalized...");
-
-            CheckFolderPathsExist();
+            Games = new ObservableCollection<Game>();
+            FolderPaths = new ObservableCollection<string>(Properties.Settings.Default.FolderPaths.Cast<string>().ToArray().Skip(1));
+            AddFolderPathCommand = new CommandRunner(AddFolder);
         }
 
-        private void CheckFolderPathsExist()
+        private void AddFolder(object obj)
         {
-            var folderPaths = Properties.Settings.Default.FolderPaths.Cast<string>().ToArray();
-            Console.WriteLine(folderPaths.Length);
-
-            if (folderPaths.Length < 2)
-            {
-                Console.WriteLine("No game folders found...");
-            }
+            FolderPaths.Add(Helper.SelectDir());
         }
     }
 }

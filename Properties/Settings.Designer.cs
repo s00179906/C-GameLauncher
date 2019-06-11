34351d971715722b50b43b561e237624cba5e0f2
hignz <higz-06@hotmail.com>
2019-06-11 04:06:47 +0100

    project structure
    basic xaml layout
    
    command
    
    theme support

--- Properties/Settings.Designer.cs
+++ Properties/Settings.Designer.cs
@@ -8,22 +8,68 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace GameLauncher.Properties
-{
-
-
+namespace GameLauncher.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
-
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.1.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
-        public static Settings Default
-        {
-            get
-            {
+        
+        public static Settings Default {
+            get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("<?xml version=\"1.0\" encoding=\"utf-16\"?>\r\n<ArrayOfString xmlns:xsi=\"http://www.w3." +
+            "org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\">\r\n  <s" +
+            "tring xsi:nil=\"true\" />\r\n</ArrayOfString>")]
+        public global::System.Collections.Specialized.StringCollection FolderPaths {
+            get {
+                return ((global::System.Collections.Specialized.StringCollection)(this["FolderPaths"]));
+            }
+            set {
+                this["FolderPaths"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        public bool FoldersSet {
+            get {
+                return ((bool)(this["FoldersSet"]));
+            }
+            set {
+                this["FoldersSet"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("Blue")]
+        public string Accent {
+            get {
+                return ((string)(this["Accent"]));
+            }
+            set {
+                this["Accent"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("Dark")]
+        public string Theme {
+            get {
+                return ((string)(this["Theme"]));
+            }
+            set {
+                this["Theme"] = value;
             }
         }
     }
---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace GameLauncher.ViewModels
+{
+    public class MainViewModel
+    {
+
+        public MainViewModel()
+        {
+            Console.WriteLine("View Model Initalized...");
+
+            CheckFolderPathsExist();
+        }
+
+        private void CheckFolderPathsExist()
+        {
+            var folderPaths = Properties.Settings.Default.FolderPaths.Cast<string>().ToArray();
+            Console.WriteLine(folderPaths.Length);
+
+            if (folderPaths.Length < 2)
+            {
+                Console.WriteLine("No game folders found...");
+            }
+        }
+    }
+}

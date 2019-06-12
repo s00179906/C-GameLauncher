88e8a43cefa3e97d87924261eae4ce782b8cb9dd
hignz <higz-06@hotmail.com>
2019-06-12 17:12:04 +0100

    detect steam paths

---
+++ Utils/GameScanner.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace GameLauncher.Utils
+{
+    public class GameScanner
+    {
+        public readonly string Steam32 = "SOFTWARE\\VALVE\\";
+        public readonly string Steam64 = "SOFTWARE\\Wow6432Node\\Valve\\";
+
+        public List<string> GameDirectories { get; set; }
+
+        public GameScanner()
+        {
+            GameDirectories = new List<string>();
+        }
+
+        public void Scan()
+        {
+            RegistryKey key = string.IsNullOrEmpty(Registry.LocalMachine.OpenSubKey(Steam64).ToString()) 
+                ? Registry.LocalMachine.OpenSubKey(Steam32) 
+                : Registry.LocalMachine.OpenSubKey(Steam64);
+
+            foreach (string k in key.GetSubKeyNames())
+            {
+                using (RegistryKey subKey = key.OpenSubKey(k))
+                {
+                    string steamPath = subKey.GetValue("InstallPath").ToString();
+                    string configPath = steamPath + "/steamapps/libraryfolders.vdf";
+                    if (File.Exists(configPath))
+                    {
+                        IEnumerable<string> configLines = File.ReadAllLines(configPath)
+                            .Where(l => !string.IsNullOrEmpty(l) && l.Contains(":/"));
+                        foreach (var line in configLines)
+                        {
+                            GameDirectories.Add($"{line}\\steamapps\\common");
+                        }
+                        GameDirectories.Add($"{steamPath}\\steamapps\\common");
+                    }
+                }
+            }
+            //string[] folders = Directory.GetDirectories(GameDirectories[0]);
+            //List<string> folderNames = new List<string>();
+
+            //foreach (var item in folders)
+            //{
+            //    int lastSlashIndex = item.LastIndexOf("\\");
+            //    folderNames.Add(item.Substring(++lastSlashIndex, item.Length - lastSlashIndex));
+            //}
+
+            //string[] files = Directory.GetFiles(GameDirectories[0], "*.exe", SearchOption.AllDirectories);
+            //ObservableCollection<Game> games = new ObservableCollection<Game>();
+
+            //List<string> fileNames = new List<string>();
+
+            //foreach (var file in files)
+            //{
+            //    int lastSlashIndex = file.LastIndexOf("\\");
+            //    fileNames.Add(file.Substring(++lastSlashIndex, file.Length - lastSlashIndex));
+            //}
+
+            //foreach (var file in fileNames)
+            //{
+            //    foreach (var folder in folderNames)
+            //    {
+            //        if (file.Trim().ToLower().Contains(folder.Trim().ToLower()))
+            //        {
+            //            Console.WriteLine(file);
+            //        }
+            //    }
+            //}
+        }
+    }
+}
--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -1,30 +1,29 @@
 ﻿using GameLauncher.Models;
 using GameLauncher.Utils;
-using Microsoft.WindowsAPICodePack.Dialogs;
-using System;
 using System.Collections.ObjectModel;
-using System.Linq;
 
 namespace GameLauncher.ViewModels
 {
     public class MainViewModel
     {
         public ObservableCollection<Game> Games { get; set; }
-        public ObservableCollection<string> FolderPaths { get; set; }
         public CommandRunner AddFolderPathCommand { get; set; }
-
+        public GameScanner Scanner { get; set; }
 
         public MainViewModel()
         {
-            Console.WriteLine("View Model Initalized...");
             Games = new ObservableCollection<Game>();
-            FolderPaths = new ObservableCollection<string>(Properties.Settings.Default.FolderPaths.Cast<string>().ToArray().Skip(1));
+
             AddFolderPathCommand = new CommandRunner(AddFolder);
+
+            Scanner = new GameScanner();
+
+            Scanner.Scan();
         }
 
         private void AddFolder(object obj)
         {
-            FolderPaths.Add(Helper.SelectDir());
+            Scanner.GameDirectories.Add(Helper.SelectDir());
         }
     }
 }

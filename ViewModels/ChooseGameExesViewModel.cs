0057e5375ab3415a23d5ef751031b9f54a66583e
ghaff <ghaff@LAPTOP-OUUVA253>
2019-06-13 17:20:23 +0100

    added new window to allow users to pick correct exe for games and viewmodel for the window.

---
+++ ViewModels/ChooseGameExesViewModel.cs
@@ -0,0 +1,31 @@
+﻿using GameLauncher.Models;
+using GameLauncher.Utils;
+using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Data;
+using GameLauncher.Views;
+
+namespace GameLauncher.ViewModels
+{
+    public class ChooseGameExesViewModel
+    {
+        public Game Game { get; set; }
+        public ObservableCollection<string> GameExecutables { get; set; }
+        public string SelectedExecutable { get; set; }
+        public ChooseGameExesViewModel(Game game)
+        {
+            Game = game;
+            GameExecutables = new ObservableCollection<string>();
+            foreach (var exe in Game.Executables)
+            {
+                GameExecutables.Add(exe);
+            }
+            
+        }
+    }
+
+   
+}

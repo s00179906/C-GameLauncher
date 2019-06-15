aad47b072993fbe486250925603faebaecd4bbf3
ghaff <ghaff@LAPTOP-OUUVA253>
2019-06-15 12:56:25 +0100

    Added logic to read from acf file to get selected games id to run from steam command to prevent games being run outside of steam.

---
+++ Utils/ReadACF.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.Win32;
+using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Windows;
+using GameLauncher.ViewModels;
+
+namespace GameLauncher.Utils
+{
+    public class ReadACF
+    {
+        public string GameName { get; set; }
+        public string GameID { get; set; }
+        public string ACFLocationPath { get; set; }
+
+        private readonly string Steam32 = "SOFTWARE\\VALVE\\STEAM";
+        private readonly string Steam64 = "SOFTWARE\\Wow6432Node\\Valve\\STEAM";
+
+        public ReadACF(string gameName)
+        {
+            GetSteamACFDirectory();
+            GetACF(gameName);
+        }
+
+        public void GetSteamACFDirectory()
+        {
+            try
+            {
+                RegistryKey key = string.IsNullOrEmpty(Registry.LocalMachine.OpenSubKey(Steam64).ToString())
+                    ? Registry.LocalMachine.OpenSubKey(Steam32)
+                    : Registry.LocalMachine.OpenSubKey(Steam64);
+
+                string path = key.GetValue("InstallPath").ToString();
+                ACFLocationPath = path + "\\steamapps";
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        public void GetACF(string gameName)
+        {
+            string[] acfFiles = Directory.GetFiles(ACFLocationPath, "*.acf");
+
+            foreach (var acfFile in acfFiles)
+            {
+                ReadACFFile(acfFile, gameName);
+            }
+        }
+
+        private void ReadACFFile(string acfFile, string _gameName)
+        {
+            try
+            {
+                var gameID = File.ReadLines(acfFile).Skip(2).Take(1).First();
+                var gameName = File.ReadLines(acfFile).Skip(6).Take(1).First();
+
+                var charsToRemove = new string[] { "\"", "\t", "installdir" };
+
+                foreach (var c in charsToRemove)
+                    gameName = gameName.Replace(c, string.Empty);
+
+                gameID = Regex.Match(gameID, @"\d+").Value;
+
+                GameID = gameID;
+                GameName = gameName;
+
+                if (_gameName == GameName)
+                {
+                    MainViewModel.GameID = int.Parse(GameID);
+                }
+            }
+            catch
+            {
+                //
+            }
+        }
+    }
+}
